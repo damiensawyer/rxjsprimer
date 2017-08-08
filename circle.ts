ebddc216e95ccd342811c2b0372fd9f6f4fa9527
Damien Sawyer <damiensawyer@gmail.com>
2017-08-08 13:48:38 +0100

    cursor working

--- circle.ts
+++ circle.ts
@@ -2,7 +2,13 @@ import { Observable } from 'rxjs';
 export class Circle {
     constructor() {
         console.log('in ciricle constructor');
+        this.bindToCircle();
+    }
+
+    bindToCircle() {
+        console.log('in bind to Circle');
         let circle = document.getElementById('circle');
+        console//.log(circle);
         let source = Observable.fromEvent(document, "mousemove")
             .map((e: MouseEvent) => {
                 return {
@@ -10,8 +16,16 @@ export class Circle {
                     y: e.clientY
                 }
             })
-            .filter(value => value.x < 100);
-        source.subscribe(x => console.log(x));
+            .delay(10)
+            .filter(value => value.x < 1000);
+        source.subscribe(x => {
+            console.log(`yay ${x}`);
+            circle.style.left = x.x.toString();
+            circle.style.top = x.y.toString();
+
+
+        });
+
     }
 };
 
