11016e379fd9e18c8744ae069118ba1a914db226
Damien Sawyer <damiensawyer@gmail.com>
2017-08-08 05:31:39 +0100

    observer

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-import {Observable} from 'rxjs';
+import {Observable, Observer} from 'rxjs';
 //alert('hello ddd');
 let numbers = [1,2,3];
 let source = Observable.from(numbers);
 source.subscribe(x => console.log(x));
 
+// or you can define an observer class... very formal
+
+class MyObserver implements Observer<number>
+{
+  next(value:any){console.log(`received ${value}`)}
+  error(e){}
+  complete(){}
+
+}
+
+source.subscribe(new MyObserver());
